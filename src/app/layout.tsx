1621687e79e821fa42872e5e7f05aacf6376d147
Qrlive <contact@qrlive.in>
2025-11-20 14:38:50 +0000

    make sure our app is ready to deploy at qrlive-menu firebase backend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
 import './globals.css';
+
+const inter = Inter({subsets: ['latin'], variable: '--font-inter'});
 
 export const metadata: Metadata = {
   title: 'Qrlive-Menu',
@@ -12,12 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-      </head>
+    <html lang="en" className={`${inter.variable}`}>
       <body className="font-body antialiased">{children}</body>
     </html>
   );
--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBYvN5KHN63UOoZnIu0z3k1izTQ8vPLzsw",
   authDomain: "studio-9558763791-119a3.firebaseapp.com",
   projectId: "studio-9558763791-119a3",
-  storageBucket: "studio-9558763791-119a3.appspot.com",
+  storageBucket: "studio-9558763791-119a3.firebasestorage.app",
   messagingSenderId: "971263048940",
   appId: "1:971263048940:web:262d4592794016d43ef9c9"
 };
