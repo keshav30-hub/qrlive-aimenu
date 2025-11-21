03af6f39f92a3a6d44c6795a197dd44b93a37bf3
Qrlive <contact@qrlive.in>
2025-11-21 14:19:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9558763791-119a3",
+  "appId": "1:971263048940:web:6826bcc90faf7d003ef9c9",
+  "apiKey": "AIzaSyBYvN5KHN63UOoZnIu0z3k1izTQ8vPLzsw",
+  "authDomain": "studio-9558763791-119a3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "971263048940"
+};
