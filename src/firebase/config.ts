4cb2fc3951b8a4456819f20e8d64cfa43eb469ca
Qrlive <contact@qrlive.in>
2025-11-24 07:59:29 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9558763791-119a3",
   "appId": "1:971263048940:web:6826bcc90faf7d003ef9c9",
+  "storageBucket": "studio-9558763791-119a3.appspot.com",
   "apiKey": "AIzaSyBYvN5KHN63UOoZnIu0z3k1izTQ8vPLzsw",
   "authDomain": "studio-9558763791-119a3.firebaseapp.com",
   "measurementId": "",
